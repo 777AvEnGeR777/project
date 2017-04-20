bdaaf505b8522b3de1f8084774d2c8749c1981ad
tp-cpp-2017-11 <keytoherheart@mail.ru>
2017-04-20 11:19:39 +0300

    Fixed data.cpp

--- Source/Data.cpp
+++ Source/Data.cpp
@@ -1,5 +1,9 @@
 #include "Data.h"
 
+std::map<std::string, Account> Data::accountList;
+std::map<std::string,Card> Data::cardList;
+std::string Data::masterHash;
+
 void Data::Save() {
 	// TODO
 	// Parser.Wrap();
